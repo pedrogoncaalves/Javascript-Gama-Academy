87261e1a0c76e069e409f05dfbe9bead5450e31d
pedrogoncaalves <pedro2001goncalves@hotmail.com>
2022-06-09 18:34:04 -0300

    conclusao primeira atividade

--- primeiroscomandos.js
+++ primeiroscomandos.js
@@ -35,3 +35,41 @@ function podeDirigir(idade) {
 }
 podeDirigir(16)
 
+
+
+
+
+function converteEmHoras (minutos) {
+    return minutos / 60
+}
+
+console.log(converteEmHoras(240))
+
+
+for (let i = 0; i <= 20; i++){
+    console.log(`i =  ${i}`)
+}
+
+function apenasImpares () {
+    for (let i = 0; i <= 20; i++) {
+        if (i % 2 !== 0  ) {
+            console.log('Aqui temos apenas os números impares ' + i)
+        }
+    }
+}
+apenasImpares()
+
+
+
+
+// FUNÇÃO TABUADA
+
+
+function tabuada (n) {
+    for (let i = 0; i<= 10; i++) {
+        const res = n * i;
+        console.log('O número que você digitou é '+ n ,'e a sua tabuada é ' + res)
+    }
+    
+}
+tabuada(2)
